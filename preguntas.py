03118808653402801aee2e8ca923e22b72624f2e
Juan Sebastian Mendoza <jmendozaco@unal.edu.co>
2022-05-22 20:38:31 -0500

    .
    .

--- preguntas.py
+++ preguntas.py
@@ -115,7 +115,7 @@ def pregunta_03():
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
     param_grid = {
-        'selectKBest': (1, 11),
+        'selectKBest_0': (1, 11),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
